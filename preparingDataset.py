bf9c680c3119271c991568d2325055369c422287
Daniel <danielisaacjuarez@gmail.com>
2024-03-10 00:09:52 -0600

    cambio

--- preparingDataset.py
+++ preparingDataset.py
@@ -3,7 +3,7 @@ import pandas as pd
 print("start")
 
 column_name = 'body' 
-df = pd.read_csv('redditClimateChangeDataset.csv', usecols=[column_name])
+df = pd.read_csv('datasetCompleto.csv', usecols=[column_name])
 print("column work done")
 
 df[column_name] = df[column_name].replace('\n', '\u00A0', regex=True)
@@ -19,5 +19,10 @@ file_chunks = [df[i:i+rows_per_file] for i in range(0, len(df), rows_per_file)]
 for i, chunk in enumerate(file_chunks):
     chunk.to_csv(f'outCleared_{i}.csv', index=False)
     print(f'outCleared_{i}.csv done')
+    chunk.to_csv(f'outCleared_{i}.txt', index=False, sep='\t')
+    print(f'outCleared_{i}.txt done')
+
+df.to_csv('filtered_text_file.txt', index=False, sep='\t')
+print("filtered_text_file.txt done")
 
 print("done")
